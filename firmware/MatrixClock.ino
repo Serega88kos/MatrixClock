c6cf47e40497cea7f51ba8f46553cddb37168c63
Sergey <57561983+Serega88kos@users.noreply.github.com>
2023-07-03 21:26:00 +0300

    v1.0

--- firmware/MatrixClock.ino
+++ firmware/MatrixClock.ino
@@ -2,21 +2,21 @@
 //Обсуждение работы на форуме https://community.alexgyver.ru/threasensor/bolshie-chasy-64x32-esp32.6716/
 //Поблагодарить за труд, можно на Яндекс 410014148046232
 
-#define GH_NO_MQTT      // MQTT
+#define GH_NO_MQTT  // MQTT
 #include <GyverHub.h>
 GyverHub hub("MyDevices", "MatrixClock", "");
 
 #include "Constants.h"
-#define ssidAP  "Clock_ESP"
-#define passAP  "adminadmin"          // не менее 8 символов
+#define ssidAP "MatrixClock"
+#define passAP "adminadmin"  // не менее 8 символов
 // настройка фоторезистора
-#define BRI_PIN             33        // PIN фоторезистора
+#define BRI_PIN 33  // PIN фоторезистора
 // настройка панели
 #include "Adafruit_GFX.h"
 #include "ESP32-HUB75-MatrixPanel-I2S-DMA.h"
-#define PANEL_RES_X 64                // ширина панели
-#define PANEL_RES_Y 32                // высота панели
-#define PANEL_CHAIN 1                 // кол-во
+#define PANEL_RES_X 64  // ширина панели
+#define PANEL_RES_Y 32  // высота панели
+#define PANEL_CHAIN 1   // кол-во
 MatrixPanel_I2S_DMA *display = nullptr;
 // шрифты
 //#include "fonts/asm_12x7b.h"
@@ -28,7 +28,7 @@ MatrixPanel_I2S_DMA *display = nullptr;
 GyverBME280 bmp;
 
 #include "microDS18B20.h"
-MicroDS18B20 <0> sensor;
+MicroDS18B20<0> sensor;
 
 #include <WiFi.h>
 #include "FS.h"
@@ -47,11 +47,12 @@ int new_bright, new_bright_f;
 unsigned long bright_timer, off_timer;
 uint8_t tab = 0;
 
-#include "EEManager.h"
-EEManager mem1(w);
-EEManager mem2(c);
-EEManager mem3(o);
-EEManager mem4(m);
+#include <FileData.h>
+
+FileData wifi_(&LittleFS, "/wifi.dat", 'A', &w, sizeof(w));
+FileData clock_(&LittleFS, "/clock.dat", 'A', &c, sizeof(c));
+FileData other_(&LittleFS, "/other.dat", 'A', &o, sizeof(o));
+FileData narod_(&LittleFS, "/narod.dat", 'A', &m, sizeof(m));
 
 GHcolor color_minus(0, 0, 255);
 GHcolor color_press(255, 0, 255);
@@ -61,21 +62,21 @@ GHcolor color_street(0, 191, 255);
 GHcolor color_text(250, 240, 230);
 
 // настройка цветов
-uint16_t minus_col   = display->color565(color_minus.r, color_minus.g, color_minus.b);  // знак минус темп-ры
-uint16_t press_col   = display->color565(color_press.r, color_press.g, color_press.b);  // давление
-uint16_t clock_col  = display->color565(color_clock.r, color_clock.g, color_clock.b);   // часы
-uint16_t black  = display->color565(0, 0, 0);                                           // черный цвет, ничего не горит
-uint16_t home_col = display->color565(color_home.r, color_home.g, color_home.b);        // дом темп-ра
-uint16_t street_col = display->color565(color_street.r, color_street.g, color_street.b);// уличная темп-ра
-uint16_t text_col = display->color565(color_text.r, color_text.g, color_text.b);// текст
+uint16_t minus_col = display->color565(color_minus.r, color_minus.g, color_minus.b);      // знак минус темп-ры
+uint16_t press_col = display->color565(color_press.r, color_press.g, color_press.b);      // давление
+uint16_t clock_col = display->color565(color_clock.r, color_clock.g, color_clock.b);      // часы
+uint16_t black = display->color565(0, 0, 0);                                              // черный цвет, ничего не горит
+uint16_t home_col = display->color565(color_home.r, color_home.g, color_home.b);          // дом темп-ра
+uint16_t street_col = display->color565(color_street.r, color_street.g, color_street.b);  // уличная темп-ра
+uint16_t text_col = display->color565(color_text.r, color_text.g, color_text.b);          // текст
 
 void setup() {
   Serial.begin(115200);
-  EEPROM.begin(1000);
-  mem1.begin(0, 'a');
-  mem2.begin(mem1.nextAddr(), 'a');
-  mem3.begin(mem2.nextAddr(), 'a');
-  mem4.begin(mem3.nextAddr(), 'a');
+  LittleFS.begin();
+  FDstat_t stat1 = wifi_.read();
+  FDstat_t stat2 = clock_.read();
+  FDstat_t stat3 = other_.read();
+  FDstat_t stat4 = narod_.read();
   HUB75_I2S_CFG mxconfig(PANEL_RES_X, PANEL_RES_Y, PANEL_CHAIN);
   mxconfig.clkphase = false;
   mxconfig.driver = HUB75_I2S_CFG::FM6126A;
@@ -93,7 +94,10 @@ void loop() {
   hub.tick();  // обязательно тикаем тут
   hub.sendUpdate("n1,new_bright");
   BrightnessCheck();
-  mem1.tick(); mem2.tick(); mem3.tick(); mem4.tick();
+  wifi_.tick();  
+  clock_.tick(); 
+  other_.tick();
+  narod_.tick();
   ntp.tick();
   Clock();
   Dots();
@@ -109,20 +113,21 @@ void loop() {
 uint8_t mods = 0;
 void rezhim() {
   static uint32_t test = millis();
-  if ((millis() - test) < 5000) { // для теста смена каждые 5 секунд
+  if ((millis() - test) < 5000) {  // для теста смена каждые 5 секунд
     switch (mods) {
       case 0:
-        TempStreet(); Press();
+        TempStreet();
+        Press();
         break;
       case 1:
         TempHome();
-        if (o.sens_bme)Hum();
+        if (o.sens_bme) Hum();
         break;
     }
   } else {
     test = millis();
     mods++;
-    display->fillRect(0, 0, 63, 10, black); // очистка верхней части экрана, путем заливки черного цвета
+    display->fillRect(0, 0, 63, 10, black);  // очистка верхней части экрана, путем заливки черного цвета
     if (mods > 1) mods = 0;
   }
 }
--- firmware/hub.ino
+++ firmware/hub.ino
@@ -15,8 +15,7 @@ void build() {
         flag_w |= hub.Input(&w.ssid, GH_CSTR, F("SSID"), 32);
         flag_w |= hub.Pass(&w.pass, GH_CSTR, F("PASS"), 32);
         if (flag_w) {
-          mem1.updateNow();
-          ESP.restart();
+          wifi_.update();
         }
         bool flag_c = 0;
         bool flag_sync = 0;
@@ -34,7 +33,7 @@ void build() {
         hub.WidgetSize(30);
         flag_c |= hub.Input(&c.gmt, GH_UINT16, F("GMT зона"), 3);
         if (flag_c) {
-          mem2.updateNow();
+          clock_.update();
         }
       }
       break;
@@ -49,24 +48,24 @@ void build() {
 
         hub.Title(F("Яркость"));
         hub.WidgetSize(25);
-        hub.Label_(F("new_bright"), String(new_bright), F("Текущая"), GH_DEFAULT, 25);
+        hub.Label_(F("new_bright"), String(new_bright), F("Текущая"), GH_DEFAULT, 20);
         flag_o |= hub.Spinner(&o.min_bright, GH_FLOAT, F("Минимальная"), 0, 255, 1);
         flag_o |= hub.Spinner(&o.max_bright, GH_FLOAT, F("Максимальная"), 10, 255, 1);
         flag_o |= hub.Spinner(&o.brg, GH_FLOAT, F("Задержка, в сек."), 0, 30, 1);
 
         hub.Title(F("Корректировка показаний"));
         hub.WidgetSize(25);
-        hub.Label(String(FtempH), F("Комнатная"), GH_DEFAULT, 25);
+        hub.Label(String(FtempH), F("Комнатная"), GH_DEFAULT, 20);
         flag_o |= hub.Spinner(&o.cor_tempH, GH_FLOAT, GH_NO_LABEL, -9, 9, 0.1);
-        hub.Label(String(FtempS), F("Уличная"), GH_DEFAULT, 25);
+        hub.Label(String(FtempS), F("Уличная"), GH_DEFAULT, 20);
         flag_o |= hub.Spinner(&o.cor_tempS, GH_FLOAT, GH_NO_LABEL, -9, 9, 0.1);
         hub.WidgetSize(25);
-        hub.Label(String(pres), F("Давление"), GH_DEFAULT, 25);
-        flag_o |= hub.Spinner(&o.cor_pres, GH_FLOAT, GH_NO_LABEL, -9, 9, 1);
-        hub.Label(String(hum), F("Влажность"), GH_DEFAULT, 25);
-        flag_o |= hub.Spinner(&o.cor_hum, GH_FLOAT, GH_NO_LABEL, -9, 9, 1);
+        hub.Label(String(pres), F("Давление"), GH_DEFAULT, 20);
+        flag_o |= hub.Spinner(&o.cor_pres, GH_INT32, GH_NO_LABEL, -9, 9, 1);
+        hub.Label(String(hum), F("Влажность"), GH_DEFAULT, 20);
+        flag_o |= hub.Spinner(&o.cor_hum, GH_INT16, GH_NO_LABEL, -9, 9, 1);
         if (flag_o) {
-          mem3.updateNow();
+          other_.update();
         }
       }
       break;
@@ -85,12 +84,10 @@ void build() {
         flag_m |= hub.Switch(&m.nm_pres, F("Давление"));
         flag_m |= hub.Switch(&m.nm_hum, F("Влажность"));
         if (flag_m) {
-          mem4.updateNow();
+          narod_.update();
         }
       }
       break;
   }
   hub.EndWidgets();
-
-  // M_TR(GP.LABEL(""), GP.LABEL(ntp.timeString()); GP.LABEL(ntp.dateString()); , GP.ICON_BUTTON("sync", "refresh")););
 }
